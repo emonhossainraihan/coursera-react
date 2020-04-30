e51efdf9b2c5897f14b298eaa7aa29d238efdc98
emonhossainraihan <mdroy7475@gmail.com>
2020-04-30 17:14:39 +0600

    Fetch from Server

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -12,12 +12,19 @@ import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 //! bring actions
-import { addComment, fetchDishes } from '../redux/ActionCreators';
+import {
+  addComment,
+  fetchDishes,
+  fetchComments,
+  fetchPromos,
+} from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
 
 class Main extends Component {
   componentDidMount() {
     this.props.fetchDishes();
+    this.props.fetchComments();
+    this.props.fetchPromos();
   }
 
   render() {
@@ -27,9 +34,15 @@ class Main extends Component {
           //As we use middleware so there is no longer exit props.dishes
           dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
           dishesLoading={this.props.dishes.isLoading}
-          dishesErrMess={this.props.dishes.errMess}
-          //before middleware
-          promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+          dishErrMess={this.props.dishes.errMess}
+          promotion={
+            this.props.promotions.promotions.filter(
+              (promo) => promo.featured
+            )[0]
+          }
+          promoLoading={this.props.promotions.isLoading}
+          promoErrMess={this.props.promotions.errMess}
+          //without go through reducer
           leader={this.props.leaders.filter((leader) => leader.featured)[0]}
         />
       );
@@ -45,10 +58,10 @@ class Main extends Component {
           }
           isLoading={this.props.dishes.isLoading}
           errMess={this.props.dishes.errMess}
-          //before middleware
-          comments={this.props.comments.filter(
+          comments={this.props.comments.comments.filter(
             (comment) => comment.dishId === parseInt(match.params.dishId, 10)
           )}
+          commentsErrMess={this.props.comments.errMess}
           addComment={this.props.addComment}
         />
       );
@@ -104,6 +117,8 @@ const mapDispatchToProps = (dispatch) => ({
   resetFeedbackForm: () => {
     dispatch(actions.reset('feedback'));
   },
+  fetchComments: () => dispatch(fetchComments()),
+  fetchPromos: () => dispatch(fetchPromos()),
 });
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
