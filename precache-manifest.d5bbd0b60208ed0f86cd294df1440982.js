4cc538a0b98e23bf30b71444bb1c9a71d33a725c
EmonHR <mdemon7475@gmail.com>
2021-01-09 14:06:00 +0600

    Updates

---
+++ precache-manifest.d5bbd0b60208ed0f86cd294df1440982.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a6c427033b8e205c321f256640665d",
+    "url": "/coursera-react/index.html"
+  },
+  {
+    "revision": "9984e8a9a9eb854297ed",
+    "url": "/coursera-react/static/css/2.2e662a52.chunk.css"
+  },
+  {
+    "revision": "8e344fe98b1317b16f24",
+    "url": "/coursera-react/static/css/main.495fa6ef.chunk.css"
+  },
+  {
+    "revision": "9984e8a9a9eb854297ed",
+    "url": "/coursera-react/static/js/2.8ea0e6a3.chunk.js"
+  },
+  {
+    "revision": "1d9ed9265a32ea4776472f445cf2df78",
+    "url": "/coursera-react/static/js/2.8ea0e6a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e344fe98b1317b16f24",
+    "url": "/coursera-react/static/js/main.96b47285.chunk.js"
+  },
+  {
+    "revision": "f42e2ca3ede1c1dcd926",
+    "url": "/coursera-react/static/js/runtime-main.faf64f1e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/coursera-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/coursera-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/coursera-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/coursera-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/coursera-react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
